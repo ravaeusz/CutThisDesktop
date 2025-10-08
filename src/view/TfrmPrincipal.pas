8839071a47ecca0f1d1d43a6c13f4a5dbcd1c95a
ravaeusz <girardirafael2003@gmail.com>
2025-10-08 13:02:46 -0300

    feat: tela de login e user model

---
+++ src/view/TfrmPrincipal.pas
@@ -0,0 +1,40 @@
+unit TfrmPrincipal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TFormularioPrincipal = class(TForm)
+    procedure FormShow(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormularioPrincipal: TFormularioPrincipal;
+
+implementation
+
+{$R *.dfm}
+
+uses TfrmLogin;
+
+procedure TFormularioPrincipal.FormShow(Sender: TObject);
+var
+login : TFormularioLogin;
+begin
+login := TFormularioLogin.Create(nil);
+try
+login.ShowModal;
+finally
+login.Free;
+end;
+
+end;
+
+end.
